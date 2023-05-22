8c7cdeaa58e5ac6eff4ffd49259a92d067722f94
Juan Sebastian Sotomayor <juanse.sotomayor77@gmail.com>
2023-05-22 11:00:36 -0500

    Quality Gate8

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -2,27 +2,33 @@ import PropTypes from "prop-types";
 import user from "../assets/user.png";
 
 export const MyCard = ({ name, birthday, email, phone, position }) => {
+  if (1 === 1) {
+    console.log("This should not always execute!");
+  } else {
+    console.log("This should never execute!");
+  }
+
   return (
-    <div className='card flex align-items-center justify-content-center gap-4 border-card'>
-      <div className='card-header'>
-        <img src={user} alt='user' width='100px' />
+    <div className="card flex align-items-center justify-content-center gap-4 border-card">
+      <div className="card-header">
+        <img src={user} alt="user" width="100px" />
       </div>
       <div>
-        <h2 className='bago-primary'>{name}</h2>
-        <div className='flex align-items-center gap-2'>
-          <i className='pi pi-calendar bago-primary' />
+        <h2 className="bago-primary">{name}</h2>
+        <div className="flex align-items-center gap-2">
+          <i className="pi pi-calendar bago-primary" />
           <p>{birthday}</p>
         </div>
-        <div className='flex align-items-center gap-2'>
-          <i className='pi pi-envelope bago-primary' />
+        <div className="flex align-items-center gap-2">
+          <i className="pi pi-envelope bago-primary" />
           <p>{email}</p>
         </div>
-        <div className='flex align-items-center gap-2'>
-          <i className='pi pi-phone bago-primary' />
+        <div className="flex align-items-center gap-2">
+          <i className="pi pi-phone bago-primary" />
           <p>{phone}</p>
         </div>
-        <div className='flex align-items-center gap-2'>
-          <i className='pi pi-briefcase bago-primary' />
+        <div className="flex align-items-center gap-2">
+          <i className="pi pi-briefcase bago-primary" />
           <p>{position}</p>
         </div>
       </div>
