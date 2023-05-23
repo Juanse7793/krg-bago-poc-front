d34f78ace5e0a81f08684ee14b373dc6be683393
Juan Sebastian Sotomayor <juanse.sotomayor77@gmail.com>
2023-05-23 18:36:41 -0500

    Fetch2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
-import cumpleaños from '../assets/happy.svg';
-import { MyCard } from '../components/MyCard';
-import { getEmployeesBirthdays } from '../service/happyService';
+import { useEffect, useState } from "react";
+import cumpleaños from "../assets/happy.svg";
+import { MyCard } from "../components/MyCard";
+import { getEmployeesBirthdays } from "../service/happyService";
 
 export const Home = () => {
   const [employees, setEmployees] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchEmployees = async () => {
       const data = await getEmployeesBirthdays();
       setEmployees(data);
-      setLoading(false);
+      setLoading(true);
     };
     fetchEmployees();
   }, []);
 
   return (
-    <div style={{ marginLeft: '17.5%' }}>
+    <div style={{ marginLeft: "17.5%" }}>
       <div className="p-col-12 flex flex-column align-items-center justify-content-center">
         <img src={cumpleaños} alt="cumpleaños" width="300px" />
         <h1>Cumpleañeros del mes</h1>
